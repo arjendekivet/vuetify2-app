7505cd1988f1d642101f611577fd7527f1c3f25f
Arjen73 <arjendekivet@gmail.com>
2021-12-08 08:58:04 +0100

    add typescript

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import VueCompositionAPI from '@vue/composition-api'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import VueCompositionAPI from "@vue/composition-api";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify.ts";
 
-Vue.use(VueCompositionAPI)
+Vue.use(VueCompositionAPI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
